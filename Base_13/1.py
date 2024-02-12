9ff53eb76e883d324f2252fe954f0afad4377a1f
sar-bc <sar-bc@mail.ru>
2024-02-12 21:43:30 +0400

    add

--- Base_13/1.py
+++ Base_13/1.py
@@ -1,23 +1,21 @@
-db = {
-    1: {'city': 'Москва', 'counter': 0},
-    2: {'city': 'Сочи', 'counter': 0},
-}
-
-
 def func(city):
-    for k, v in db.items():
-        # print(k, "->", v.get('city'))
-        if city == v.get('city'):
-            counter = v.get('counter') + 1
-            db[k]['counter'] = counter
-            return print(v.get('city'), counter)
-        else:
-            print("Такого города нет")
+    count = 0
+
+    def inner():
+        nonlocal count
+        count += 1
+        print(city, count)
+
+    return inner
 
 
-print(func("Москва"))
-print(func("Москва"))
-print(func("Сочи"))
-print(func("Сочи"))
-print(func("Москва"))
+res1 = func("Москва")
+res1()
+res1()
+
+res2 = func("Сочи")
+res2()
+res2()
+
+res1()
 
