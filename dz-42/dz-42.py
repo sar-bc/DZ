5ee9950c4a602e33a27d7a135b396621c65d748d
sar-bc <sar-bc@mail.ru>
2024-03-18 22:36:11 +0400

    dz-48

--- dz-42/dz-42.py
+++ dz-42/dz-42.py
@@ -5,5 +5,11 @@ for k in os.listdir("files"):
     lst.append(fr"files\{k}")
 
 print(sorted(lst, reverse=True))
-
-
+print("*" *20)
+root = "files"
+objs = os.listdir(root)
+print(objs)
+objs = list(map(lambda i: os.path.join(root, i), objs))
+print(objs)
+objs_sort = sorted(objs, key=os.path.isfile, reverse=True)
+print(objs_sort)
