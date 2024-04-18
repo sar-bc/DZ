14efb5fdcb89a527362018b3e105589edaed942f
Виктор <sar-bc@mail.ru>
2024-04-19 01:22:10 +0400

    dz_65

--- dz_65/dz_65.py
+++ dz_65/dz_65.py
@@ -4,3 +4,12 @@ import csv
 
 response = requests.get("https://jsonplaceholder.typicode.com/todos")
 todos = json.loads(response.text)
+
+with open("json.csv", "w") as f:
+    writer = csv.DictWriter(f, delimiter=';', lineterminator='\r', fieldnames=list(todos[0].keys()))
+    writer.writeheader()
+    for w in todos:
+        writer.writerow(w)
+    print("Запись в файл выполнена.")
+
+        
