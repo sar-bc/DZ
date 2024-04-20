66e7540b9e0af24fec8073a7f93fddecaa3ab9a3
sar-bc <sar-bc@mail.ru>
2024-04-20 20:14:48 +0400

    add

--- dz_65/dz_65.py
+++ dz_65/dz_65.py
@@ -4,7 +4,7 @@ import csv
 
 response = requests.get("https://jsonplaceholder.typicode.com/todos")
 todos = json.loads(response.text)
-
+print(todos)
 with open("json.csv", "w") as f:
     writer = csv.DictWriter(f, delimiter=';', lineterminator='\r', fieldnames=list(todos[0].keys()))
     writer.writeheader()
