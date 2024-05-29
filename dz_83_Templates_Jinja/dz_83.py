298d12a8e01567092a236f4f117bd281785d6bbb
sar-bc <sar-bc@mail.ru>
2024-05-29 23:09:18 +0400

    dz_83

---
+++ dz_83_Templates_Jinja/dz_83.py
@@ -0,0 +1,12 @@
+from jinja2 import Template
+
+
+html = """
+{% macro get_input(type='text', name='', placeholder='') %}
+    <input type="{{type}}" name="{{name}}"  placeholder="{{placeholder}}">
+{% endmacro %}
+
+<p>{{get_input('username')}}</p>
+"""
+
+
