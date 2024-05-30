e47d01adde5c626d8459c155a57a3a0c520f9f89
sar-bc <sar-bc@mail.ru>
2024-05-30 18:59:10 +0400

    edit dz_83

--- dz_83_Templates_Jinja/dz_83.py
+++ dz_83_Templates_Jinja/dz_83.py
@@ -2,11 +2,18 @@ from jinja2 import Template
 
 
 html = """
-{% macro get_input(type='text', name='', placeholder='') %}
+{%- macro get_input(type='text', name='', placeholder='') -%}
     <input type="{{type}}" name="{{name}}"  placeholder="{{placeholder}}">
-{% endmacro %}
+{%- endmacro -%}
 
-<p>{{get_input('username')}}</p>
+<p>{{get_input(name='firstname', placeholder='Имя')}}</p>
+<p>{{get_input(name='lastname', placeholder='Фамилия')}}</p>
+<p>{{get_input(name='address', placeholder='Адрес')}}</p>
+<p>{{get_input(type='tel', name='phone', placeholder='Телефон')}}</p>
+<p>{{get_input(type='email', name='email', placeholder='Почта')}}</p>
 """
 
+tm = Template(html)
+msg = tm.render()
 
+print(msg)
